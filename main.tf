d47322ca48d0d2450e5d037623180321b400b96b
zkan <kan@prontomarketing.com>
2019-05-29 18:05:34 +0700

    :sparkles: Create instance and allow HTTP :bear:

--- main.tf
+++ main.tf
@@ -1,5 +1,42 @@
 provider "google" {
+  credentials = "${file("prontotools-9ee97f835c35.json")}"
   project = "prontotools-212000"
   region  = "asia-northeast2"
   zone    = "asia-northeast2-a"
+}
+
+resource "google_compute_instance" "tf_instance" {
+  name         = "tf-instance"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20190514"
+      size  = 30
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  tags = ["my-web"]
+}
+
+resource "google_compute_firewall" "my-http-server" {
+  name    = "tf-default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["my-web"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.tf_instance.network_interface.0.access_config.0.nat_ip}"
 }
